7933d245d5910f0ca616c5621ffd29e0deadcfc4
Pierre Emad <68783676+Pierre-Emad@users.noreply.github.com>
2023-08-14 21:34:56 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,9 +5,7 @@
 void main(void)
 {
 	int i;
-	int seven_seg[10] = {0b00111111, 0b00000110, 0b01011011, 0b01001111,
-					0b01100110, 0b01101101, 0b01111101, 0b00000111,
-					0b01111111, 0b01101111};
+	int seven_seg[10] = {0b00111111, 0b00000110, 0b01011011, 0b01001111, 0b01100110, 0b01101101, 0b01111101, 0b00000111, 0b01111111, 0b01101111};
 	DDRA = 0b00000111; //pins A0, A1, A2: output for LEDS
 	DDRC = 0b01111111; //pins C0 - C6: output for left 7-seg
 	DDRD = 0b01111111; //pins D0 - D6: output for right 7-seg
